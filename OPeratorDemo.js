1973a0b20a72793e26e4c0e35fc0deb5280e4701
AravindBalaji1990 <aravindbalaji.balaji7@gmail.com>
2024-01-18 19:39:43 +0300

    latest code

---
+++ OPeratorDemo.js
@@ -0,0 +1,15 @@
+//Operator demo
+
+var a ='name'
+console.log(a)
+console.log(typeof a)
+var b ="name1"
+console.log(b)
+console.log(typeof b)
+var c =2
+console.log(typeof c)
+console.log(1=='1');// true -> it checks for the data equals or not
+console.log(1==='1')// false -> it chekcs the data as well as data type
+
+console.log(1==='2')// false
+console.log(1==2)// false
---
+++ VariableTypes.js
@@ -0,0 +1,15 @@
+//var and let willa ct same but there is a scope difference
+// var can be accessible anywhere
+// let can be accessible within the scope
+var a =10;
+a=100;
+console.log(a)
+
+let b =20;
+b=200;
+console.log(b)
+
+// const means you can not change the variable
+const c =30;
+// c=300
+console.log(c)
