efb15ba2035e807e8e076dd6f4eb04b12200d50a
AravindBalaji1990 <aravindbalaji.balaji7@gmail.com>
2024-02-23 20:31:43 +0300

    test commit

--- OPeratorDemo.js
+++ OPeratorDemo.js
@@ -1,15 +1,15 @@
 //Operator demo
 
-var a ='name'
-console.log(a)
-console.log(typeof a)
-var b ="name1"
-console.log(b)
-console.log(typeof b)
-var c =2
-console.log(typeof c)
-console.log(1=='1');// true -> it checks for the data equals or not
-console.log(1==='1')// false -> it chekcs the data as well as data type
+// var a ='name'
+// console.log(a)
+// console.log(typeof a)
+// var b ="name1"
+// console.log(b)
+// console.log(typeof b)
+var c = 2
+console.log(typeof c) //number
+console.log(c =='2');// true -> it checks for the data equals or not but it does not check for the data type of the variable
+console.log(c ==='2')// false -> it chekcs the data as well as data type
 
-console.log(1==='2')// false
-console.log(1==2)// false
+console.log(1 !='1')// false not same datatype is not included
+console.log(1!==1)// false - not identical include data type as well
