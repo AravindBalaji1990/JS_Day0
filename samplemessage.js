76f7aa11f3cbbdefdecbd8f12628fd382337a004
AravindBalaji1990 <aravindbalaji.balaji7@gmail.com>
2024-04-05 19:31:07 +0300

    new chnages

--- samplemessage.js
+++ samplemessage.js
@@ -1,3 +1,7 @@
 function msgCheck(){  
-    alert("this is javascript alert");  
+    //alert("this is javascript alert");  
+    console.log("this is javascript alert");  
    }  
+
+// we are invoking the function to execute it 
+msgCheck()
